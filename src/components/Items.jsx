bbcfc1ab080a236fc37d40dbe2a10e973c19c12b
Steven <steven.acosta867@gmail.com>
2024-12-09 23:30:37 -0500

    changes

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -6,6 +6,8 @@ import item2 from '../assets/item2.png'
 import item3 from '../assets/item3.png'
 import item4 from '../assets/item4.png'
 
+import link1 from '../assets/amazon.jpg'
+import link2 from '../assets/Abercrombie.svg'
 
 
 import ItemBox from './ItemBox';
@@ -28,28 +30,28 @@ function Items() {
             price:"$24.99",
             image: item1,
             link:"https://www.amazon.com/dp/B0B3MVH6JB/?coliid=I1O1GCI8FWFX82&colid=34XEYC7ZLUSQ4&psc=1&ref_=list_c_wl_lv_ov_lig_dp_it",
-            linkimage:'./public/amazon.jpg',
+            linkimage:link1,
         },
         {
             name:"Quntis Computer Monitor Lamp, Screen Monitor Light Bar",
             price:"$39.99",
             image: item2,
             link:"https://www.amazon.com/dp/B08DKQ3JG1/?coliid=I1W0BLMOKFCUPW&colid=34XEYC7ZLUSQ4&ref_=list_c_wl_lv_ov_lig_dp_it&th=1",
-            linkimage:'./public/amazon.jpg',
+            linkimage: link1,
         },
         {
             name:"Essential Cropped Full-Zip Hoodie",
             price:"$63.99",
             image: item3,
             link:"https://www.abercrombie.com/shop/us/p/essential-cropped-full-zip-hoodie-57732341?faceout=prod&seq=01",
-            linkimage:'./public/Abercrombie.svg',
+            linkimage:link2,
         },
         {
             name: "Automatic Cat Feeders, 3L Timed Cat Dry Food Dispenser",
             price: '$32.99',
             image: item4,
             link: "https://www.amazon.com/dp/B0D3LQNCWW/?coliid=I2ZOMND5NV3OHY&colid=34XEYC7ZLUSQ4&ref_=list_c_wl_lv_ov_lig_dp_it&th=1",
-            linkimage:'./public/amazon.jpg'
+            linkimage:link1
         }
 
     ]
